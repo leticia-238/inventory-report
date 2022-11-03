3ddf31987d1814f5671209e803bf1eb8d963f1f8
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-03 19:38:43 -0300

    test:  implementa teste do relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,26 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+import pytest
 
 
-def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def product_info():
+    return {
+        "id": "1",
+        "nome_do_produto": "Nicotine Polacrilex",
+        "nome_da_empresa": "Target Corporation",
+        "data_de_fabricacao": "2021-02-18",
+        "data_de_validade": "2023-09-17",
+        "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+        "instrucoes_de_armazenamento": "instrucao 1",
+    }
+
+
+def test_relatorio_produto(product_info):
+    product = Product(*product_info.values())
+    assert product.__repr__() == (
+        "O produto Nicotine Polacrilex"
+        " fabricado em 2021-02-18"
+        " por Target Corporation"
+        " com validade até 2023-09-17"
+        " precisa ser armazenado instrucao 1."
+    )
