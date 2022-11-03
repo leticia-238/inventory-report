4c6352d184a194fe1e1d6403dfe6793735f58b87
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-03 19:21:59 -0300

    test:  implementa teste do construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+import pytest
 
 
-def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def product_info():
+    return {
+        "id": "1",
+        "nome_do_produto": "Nicotine Polacrilex",
+        "nome_da_empresa": "Target Corporation",
+        "data_de_fabricacao": "2021-02-18",
+        "data_de_validade": "2023-09-17",
+        "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+        "instrucoes_de_armazenamento": "instrucao 1",
+    }
+
+
+def test_cria_produto(product_info):
+    product = Product(*product_info.values())
+    assert product.id == product_info["id"]
+    assert product.nome_do_produto == product_info["nome_do_produto"]
+    assert product.nome_da_empresa == product_info["nome_da_empresa"]
+    assert product.data_de_fabricacao == product_info["data_de_fabricacao"]
+    assert product.data_de_validade == product_info["data_de_validade"]
+    assert product.numero_de_serie == product_info["numero_de_serie"]
+    assert (
+        product.instrucoes_de_armazenamento
+        == product_info["instrucoes_de_armazenamento"]
+    )
