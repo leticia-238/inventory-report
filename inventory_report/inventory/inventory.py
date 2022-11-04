0791085cbb4c0e7754e243850d7bdc6c353c3a50
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-04 19:55:58 -0300

    feat:   adiciona o método para importação de xml na classe Inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,20 +2,27 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xmltodict
 
 
 class Inventory:
     @classmethod
+    def read_xml(cls, path):
+        with open(path) as file:
+            content = xmltodict.parse(file.read())
+        return content["dataset"]["record"]
+
+    @classmethod
     def read_json(cls, path):
         with open(path) as file:
             content = file.read()
-            return json.loads(content)
+        return json.loads(content)
 
     @classmethod
     def read_csv(cls, path):
         with open(path) as file:
-            content = csv.DictReader(file)
-            return list(content)
+            content = list(csv.DictReader(file))
+        return content
 
     @classmethod
     def import_data(cls, path, report_type):
@@ -25,6 +32,8 @@ class Inventory:
             inventory = cls.read_csv(path)
         elif file_extension == "json":
             inventory = cls.read_json(path)
+        elif file_extension == "xml":
+            inventory = cls.read_xml(path)
 
         if report_type == "simples":
             return SimpleReport.generate(inventory)
