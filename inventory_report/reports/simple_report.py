b0bd653d57e29c70bc1aed8ad9bae74da8f17d33
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-04 15:04:39 -0300

    refactor: altera método generate na classe SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -4,26 +4,30 @@ from collections import Counter
 
 class SimpleReport:
     @classmethod
+    def stock_by_company(cls, inventory):
+        companies = [p["nome_da_empresa"] for p in inventory]
+        return Counter(companies)
+
+    @classmethod
     def generate(cls, inventory):
         current_date = datetime.now().strftime("%Y-%m-%d")
-        oldest_manufacturing_date = [
-            product["data_de_fabricacao"] for product in inventory
-        ]
-        earliest_expiration_date = [
-            product["data_de_validade"]
-            for product in inventory
-            if product["data_de_validade"] >= current_date
-        ]
-        companies = dict()
-        for product in inventory:
-            product_qtd = companies.setdefault(product["nome_da_empresa"], 0)
-            companies[product["nome_da_empresa"]] = product_qtd + 1
+        oldest_manufacturing_date = inventory[0]["data_de_fabricacao"]
+        earliest_expiration_date = inventory[0]["data_de_validade"]
 
-        company_name = Counter(companies).most_common(1)[0]
-        oldest_manufacturing_date.sort()
-        earliest_expiration_date.sort()
+        for p in inventory:
+            if p["data_de_fabricacao"] < oldest_manufacturing_date:
+                oldest_manufacturing_date = p["data_de_fabricacao"]
+            if (
+                earliest_expiration_date
+                > p["data_de_validade"]
+                >= current_date
+            ):
+                earliest_expiration_date = p["data_de_validade"]
+
+        company_name, _ = cls.stock_by_company(inventory).most_common(1)[0]
+
         return (
-            f"Data de fabricação mais antiga: {oldest_manufacturing_date[0]}\n"
-            f"Data de validade mais próxima: {earliest_expiration_date[0]}\n"
-            f"Empresa com mais produtos: {company_name[0]}"
+            f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
+            f"Data de validade mais próxima: {earliest_expiration_date}\n"
+            f"Empresa com mais produtos: {company_name}"
         )
