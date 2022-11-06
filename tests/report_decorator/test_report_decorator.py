2db1d4ebffe2ec5ab3b3b41ecf77994e93bf8da6
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-06 19:52:46 -0300

    test: implementa teste do método generate da classe ColoredReport

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,22 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from unittest.mock import patch
+
+simple_report = (
+    "Data de fabricação mais antiga: 20/07/2019\n"
+    "Data de validade mais próxima: 06/05/2023\n"
+    "Empresa com mais produtos: X Company"
+)
+
+expected_colored_report = (
+    "\033[32mData de fabricação mais antiga:\033[0m 20/07/2019\n"
+    "\033[32mData de validade mais próxima:\033[0m 06/05/2023\n"
+    "\033[32mEmpresa com mais produtos:\033[0m \033[31mX Company\033[0m"
+)
 
 
-def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+@patch("inventory_report.reports.simple_report.SimpleReport")
+def test_decorar_relatorio(SimpleReport):
+    SimpleReport.generate.return_value = simple_report
+    colored_report = ColoredReport(SimpleReport)
+    generated_report = colored_report.generate([])
+    assert generated_report == expected_colored_report
