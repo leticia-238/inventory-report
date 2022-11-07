8f896acd7a5a35a1d63b8df5c497acf3e7af1c60
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-07 17:21:03 -0300

    feat(inventory_report/main.py): implementa a função main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,35 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
 def main():
-    pass
+    try:
+        path = sys.argv[1]
+        report_type = sys.argv[2]
+    except IndexError:
+        sys.stderr.write("Verifique os argumentos\n")
+    else:
+        importers = {
+            "csv": CsvImporter,
+            "json": JsonImporter,
+            "xml": XmlImporter,
+        }
+        reports = {
+            "simples": SimpleReport,
+            "completo": CompleteReport,
+        }
+
+        file_extension = path.split(".")[1]
+        inventory = InventoryRefactor(importers[file_extension])
+        inventory.import_data(path, report_type)
+        report = reports[report_type].generate(inventory.data)
+        print(report, end="")
+
+
+if __name__ == "__main__":
+    main()
